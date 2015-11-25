84f1e8f13102a69498b150a608d65acd7bbb56a8
Alex Banbury <afbanbury@hotmail.com>
2015-11-25 22:26:43 +0000

    Initial user api with mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,134 @@
 var express = require('express');
+var User = require('./../models/user');
 var router = express.Router();
+
 
 /* GET api listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('base api');
+});
+
+/* POST authenticate listing. */
+router.post('/authenticate', function(req, res, next) {
+    // find the user
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user) {
+
+    if (err) throw err;
+
+    // no user with that username was found
+    if (!user) {
+      res.json({ 
+        success: false, 
+        message: 'Authentication failed. User not found.' 
+      });
+    } else if (user) {
+
+      // check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({ 
+          success: false, 
+          message: 'Authentication failed. Wrong password.' 
+        });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign({
+        	name: user.name,
+        	username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+/* GET api listing. */
+router.post('/createUser', function(req, res, next) {
+  
+  
+  	var user = new User();		// create a new instance of the User model
+		user.name = req.body.name;  // set the users name (comes from the request)
+		user.username = req.body.username;  // set the users username (comes from the request)
+		user.password = req.body.password;  // set the users password (comes from the request)
+
+		user.save(function(err) {
+			if (err) {
+				// duplicate entry
+				if (err.code == 11000) 
+					return res.json({ success: false, message: 'A user with that username already exists. '});
+				else 
+					return res.send(err);
+			}
+
+			// return a message
+			res.json({ message: 'User created!' });
+		});
+});
+
+router.get('/users', function(req, res) {
+ 		User.find(function(err, users) {
+			if (err) res.send(err);
+
+			// return the users
+			res.json(users);
+		}); 
+});
+
+router.get('/users/:user_id', function(req, res) {
+  
+		User.findById(req.params.user_id, function(err, user) {
+			if (err) res.send(err);
+
+			// return that user
+			res.json(user);
+		});
+});
+
+router.put('/users/:user_id', function(req, res) {
+  
+  		User.findById(req.params.user_id, function(err, user) {
+
+			if (err) res.send(err);
+
+			// set the new user information if it exists in the request
+			if (req.body.name) user.name = req.body.name;
+			if (req.body.username) user.username = req.body.username;
+			if (req.body.password) user.password = req.body.password;
+
+			// save the user
+			user.save(function(err) {
+				if (err) res.send(err);
+
+				// return a message
+				res.json({ message: 'User updated!' });
+			});
+
+		});
+});
+
+router.delete('/users/:user_id', function(req, res) {
+  User.remove({
+			_id: req.params.user_id
+		}, function(err, user) {
+			if (err) res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
 
 var config = require('./config');
 
@@ -11,6 +12,9 @@ var api = require('./routes/api');
 var pages = require('./routes/pages');
 
 var app = express();
+
+// connect to our database (hosted on modulus.io)
+mongoose.connect('mongodb://localhost/web3Cdb');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
